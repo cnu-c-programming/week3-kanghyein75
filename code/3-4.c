0ce57c91d498a6183154cb9181eadea8b4316f0f
kanghyein75 <kanghye675@gmail.com>
2026-03-20 12:12:48 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+int main()
+{
+    print("%d\n", max_of(4, 5, 12, 3, 9));
+    print("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    print("%d\n", max_of(3, 100, 200, 300));
 
     return 0;
 }
