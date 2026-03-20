a99babc9ec47ce1689d3f72badd9e4b4c29f5cca
kanghyein75 <kanghye675@gmail.com>
2026-03-20 12:09:46 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main() {
+void print() {
+    printf("Welcome to Computer Programing 03 Lab!\n");
+}
+
+int squre(int n) {
+    return n * n;
+}
+
+int main()
+{
+    print();
+    
+    int num = 5;
+    int result = squre(num);
+    printf("%d %d\n", num, result);
 
     return 0;
 }
