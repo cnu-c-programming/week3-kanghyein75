72e52e753697400b5d9b056ed84101592dd50e93
kanghyein75 <kanghye675@gmail.com>
2026-03-20 12:10:57 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
+void divide(int a, int b, int *q, int *r) {
+    
+}
+
+int main()
+{
+    int a = 27;
+    int b = 4;
+    int q = 0;
+    int r = 0;
+
+    divide(&a, &b, &q, &r);
+
+    printf("%d\n%d\n", q, r);
 
     return 0;
 }
