107b6056a219ef23513a5b7900a3d67e7c463799
kanghyein75 <kanghye675@gmail.com>
2026-03-20 12:13:03 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+int main()
+{
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 4, 10, 20, 30, 40);
 
     return 0;
 }
