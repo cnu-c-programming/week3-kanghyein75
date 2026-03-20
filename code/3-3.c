ad77ca29aa3dc4e034ad7520af236a16cc97af2c
kanghyein75 <kanghye675@gmail.com>
2026-03-20 12:12:30 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdarg.h>
 
-int main() {
+int main()
+{
+    print("%d\n", average(3, 10, 20, 30));
+    print("%d\n", average(5, 10, 20, 30, 40, 50));
 
     return 0;
 }
