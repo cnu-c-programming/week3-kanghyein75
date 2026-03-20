2d0146c061a7045311c7c8243b59192d660c1936
kanghyein75 <kanghye675@gmail.com>
2026-03-20 12:16:51 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int a = 10, b = 5;
 
-    printf("%d + %d = %d", a, b, a + b);
-    printf("%d - %d = %d", a, b, a - b);
-    printf("%d * %d = %d", a, b, a * b);
-    printf("%d / %d = %d", a, b, a / b);
-
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
 
     return 0;
 }
